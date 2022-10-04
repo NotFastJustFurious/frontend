46d1c4639e4119d8686966dabe35faa3edd528fa
porama <porama@porama.dev>
2022-10-04 19:49:25 +0700

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className={"h-30 w-30 bg-blue-300"}>Hello there</div>
+        </div>
+    );
 }
 
 export default App;
