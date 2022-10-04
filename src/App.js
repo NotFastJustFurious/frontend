cebd6f351737a3349d065f1e80e1236ece627576
porama <porama@porama.dev>
2022-10-04 19:55:36 +0700

    add tailwindcss component support

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavigationBar from "./component/NavigationBar";
 
 function App() {
     return (
         <div className="App">
+            <NavigationBar></NavigationBar>
             <div className={"h-30 w-30 bg-blue-300"}>Hello there</div>
         </div>
     );
