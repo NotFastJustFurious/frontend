040ab233f4fc14fe6898636b62554afaf7461396
porama <porama@porama.dev>
2022-11-02 21:12:30 +0700

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        colors: {
-            "furious-green": "#5DB075",
-            "furious-green-2": "#2B633B",
-            "furious-green-3": "#9FFCBA",
-            "furious-comp-1": "#632121",
-            "furious-comp-2": "#B05D5D"
+        extend: {
+            colors:{
+                "furious-green": "#5DB075",
+                "furious-green-2": "#2B633B",
+                "furious-green-3": "#9FFCBA",
+                "furious-comp-1": "#632121",
+                "furious-comp-2": "#B05D5D"
+            }
         },
-        extend: {},
     },
     plugins: [
         require('@tailwindcss/forms')
