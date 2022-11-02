a15819e01e8e13338b4d4ab86bb370eff99c3c9d
porama <porama@porama.dev>
2022-11-02 21:59:20 +0700

    fix login page colour

--- src/page/login/Login.js
+++ src/page/login/Login.js
@@ -32,7 +32,7 @@ export default function Login() {
                         </h2>
                         <p className="mt-2 text-center text-sm text-gray-600">
                             Or{' '}
-                            <a href="/register" className="font-medium text-indigo-600 hover:text-indigo-500">
+                            <a href="/register" className="font-medium text-furious-green hover:text-indigo-500">
                                 create your account now!
                             </a>
                         </p>
@@ -76,7 +76,7 @@ export default function Login() {
                                     id="remember-me"
                                     name="remember-me"
                                     type="checkbox"
-                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                    className="h-4 w-4 rounded border-gray-300 text-furious-green focus:ring-indigo-500"
                                 />
                                 <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
                                     Remember me
@@ -84,7 +84,7 @@ export default function Login() {
                             </div>
 
                             <div className="text-sm">
-                                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                <a href="#" className="font-medium text-furious-green hover:text-indigo-500">
                                     Forgot your password?
                                 </a>
                             </div>
@@ -93,11 +93,12 @@ export default function Login() {
                         <div>
                             <button
                                 type="button"
-                                className="group relative flex w-full justify-center rounded-md border border-transparent bg--600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-00 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                className="group relative flex w-full justify-center rounded-md border border-transparent bg-furious-green py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                                 onClick={onSubmit}
                             >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true"/>
+                  <LockClosedIcon className="h-5 w-5 text-furious-green-3 group-hover:text-indigo-400"
+                                  aria-hidden="true"/>
                 </span>
                                 Sign in
                             </button>
