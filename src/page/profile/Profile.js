84c42d78469f742f28caea373f145e5387241f27
porama <porama@porama.dev>
2022-11-02 19:27:43 +0700

    add card to profile & success page to register

--- src/page/profile/Profile.js
+++ src/page/profile/Profile.js
@@ -1,9 +1,28 @@
 import NavigationBar from "../../component/NavigationBar";
 
 export default function Profile() {
+
+    let messages = [
+        {
+            author: "John Doe",
+            content: "Why",
+            time: "10 minutes ago"
+        },
+        {
+            author: "John Die",
+            content: "I'd love to talk to Dr Cow more",
+            time: "12 minutes ago"
+        },
+        {
+            author: "John Die",
+            content: "Please just kill me already!",
+            time: "24 minutes ago"
+        }
+    ]
+
     return (
         <>
-            <NavigationBar ></NavigationBar>
+            <NavigationBar></NavigationBar>
             {/*
         This example requires updating your template:
 
@@ -12,34 +31,57 @@ export default function Profile() {
         <body class="h-full">
         ```
       */}
-            <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-                <div className="w-full max-w-md space-y-8">
-                    <div>
-                        {/* <img
-                            className="mx-auto h-12 w-auto"
-                            src="https://www.electrolux.co.th/globalassets/catalog/toasters--grills/e4ts1-50ss-angl-1500x1500-new.png"
-                            alt="Your Company"
-                        /> */}
-                        <img 
-                            className="mx-auto h-24 w-24 overflow-hidden rounded-full bg-gray-100 border-2 border-furious-green"
-                            src="https://www.electrolux.co.th/globalassets/catalog/toasters--grills/e4ts1-50ss-angl-1500x1500-new.png" 
-                            />
-                        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                            Firstname Lastname
-                        </h2>
-                        <p className="mt-2 text-center text-sm text-gray-600">
-                            <a href="/register" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                Patient
-                            </a>
-                        </p>
-                    </div>
-                    <div>
-                        {/* <img
+            <div className="flex flex-col min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <img
+                    className="mx-auto h-24 w-24 overflow-hidden rounded-full bg-gray-100 border-2 border-furious-green"
+                    src="https://www.electrolux.co.th/globalassets/catalog/toasters--grills/e4ts1-50ss-angl-1500x1500-new.png"
+                />
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                    Firstname Lastname
+                </h2>
+                <p className="mt-2 text-center text-sm text-gray-600">
+                    <a href="/register" className="font-medium text-indigo-600 hover:text-indigo-500">
+                        Patient
+                    </a>
+                </p>
+
+                <div className="flex flex-col mt-4">
+                    {
+                        messages.map((message) => {
+                            return <div className="flex flex-row m-2 items-center text-center p-5 rounded-2xl outline outline-2 outline-furious-green-3 hover:bg-furious-green-3 transition">
+                                <div className="min-w-fit pr-8">
+                                    <div>
+                                        <img
+                                            className="mx-auto h-12 w-auto rounded-full bg-gray-100 border-2 border-furious-green w-10 h-10"
+                                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                            alt="Your Company"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="w-full flex flex-col items-start">
+                                    <div className="text-xs w-full flex flex-row justify-between">
+                                        <div>
+                                        {message.author}
+                                        </div>
+                                        <div>
+                                            {message.time}
+                                        </div>
+                                    </div>
+                                    <div>
+                                        {message.content}
+                                    </div>
+                                </div>
+                            </div>
+                        })
+                    }
+                </div>
+
+                <div>
+                    {/* <img
                             className="rounded-full mx-auto absolute border"
                             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                             alt="Your Company"
                         /> */}
-                    </div>
                 </div>
             </div>
         </>
--- src/page/register/Register.js
+++ src/page/register/Register.js
@@ -1,115 +1,139 @@
 import NavigationBar from "../../component/NavigationBar";
+import {useState} from "react";
 
 export default function Register() {
 
-    const onSubmit = (e) => {
-        e.preventDefault();
+    const [success, setSuccess] = useState(false);
+
+    let onSubmit = (e) => {
+        setSuccess(true);
         console.log("submit");
+    };
+    onSubmit = onSubmit.bind(this);
+
+    console.log("Success: " + success);
+    if (success) {
+        setTimeout(() => {
+            window.location.href = "/profile";
+        }, 1000);
     }
 
-    return (
-        <>
-            <NavigationBar ></NavigationBar>
-            {/*
-        This example requires updating your template:
+    if (!success) {
+        return (
+            <>
+                <NavigationBar></NavigationBar>
 
-        ```
-        <html class="h-full bg-gray-50">
-        <body class="h-full">
-        ```
-      */}
-            <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-                <div className="w-full max-w-md space-y-8">
-                    <div>
-                        {/* <img
+                <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+                     hidden={success}>
+                    <div className="w-full max-w-md space-y-8">
+                        <div>
+                            {/* <img
                             className="mx-auto h-12 w-auto"
                             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                             alt="Your Company"
                         /> */}
-                        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                            Sign Up
-                        </h2>
-                    </div>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
-                        <input type="hidden" name="remember" defaultValue="true" />
-                        <div className="-space-y-px rounded-md shadow-sm">
+                            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                                Sign Up
+                            </h2>
+                        </div>
+                        <form className="mt-8 space-y-6" action="#" method="POST">
+                            <input type="hidden" name="remember" defaultValue="true"/>
+                            <div className="-space-y-px rounded-md shadow-sm">
 
-                            <div >
-                                <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
-                                    First name
-                                </label>
-                                <input
-                                    type="text"
-                                    name="first-name"
-                                    id="first-name"
-                                    autoComplete="given-name"
-                                    required
-                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                    placeholder="First name"
-                                />
+                                <div>
+                                    <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                                        First name
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="first-name"
+                                        id="first-name"
+                                        autoComplete="given-name"
+                                        required
+                                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                        placeholder="First name"
+                                    />
+                                </div>
+
+                                <div>
+                                    <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                                        Last name
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="last-name"
+                                        id="last-name"
+                                        autoComplete="family-name"
+                                        required
+                                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                        placeholder="Last name"
+                                    />
+                                </div>
+
+                                <div>
+                                    <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
+                                        Email address
+                                    </label>
+                                    <input
+                                        type="email"
+                                        name="email-address"
+                                        id="email-address"
+                                        autoComplete="email"
+                                        required
+                                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                        placeholder="Email address"
+                                    />
+                                </div>
+
+                                <div>
+                                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                                        Password
+                                    </label>
+                                    <input
+                                        id="password"
+                                        name="password"
+                                        type="password"
+                                        autoComplete="current-password"
+                                        required
+                                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                        placeholder="Password"
+                                    />
+                                </div>
+
                             </div>
-
-                            <div >
-                                <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
-                                    Last name
-                                </label>
-                                <input
-                                    type="text"
-                                    name="last-name"
-                                    id="last-name"
-                                    autoComplete="family-name"
-                                    required
-                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                    placeholder="Last name"
-                                />
-                            </div>
-
-                            <div >
-                                <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
-                                    Email address
-                                </label>
-                                <input
-                                    type="email"
-                                    name="email-address"
-                                    id="email-address"
-                                    autoComplete="email"
-                                    required
-                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                    placeholder="Email address"
-                                />
-                            </div>
-
                             <div>
-                                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                                    Password
-                                </label>
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    autoComplete="current-password"
-                                    required
-                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                    placeholder="Password"
-                                />
-                            </div>
+                                <button
+                                    type="button"
+                                    className="group relative flex w-full justify-center rounded-md border border-transparent bg--600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-00 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                    onClick={onSubmit}
+                                >
 
-                        </div>
-                        <div>
-                            <button
-                                type="submit"
-                                className="group relative flex w-full justify-center rounded-md border border-transparent bg--600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-00 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                onSubmit={onSubmit}
-                            >
                                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <div className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                                    <div className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                                         aria-hidden="true"/>
                                 </span>
-                                Sign up
-                            </button>
-                        </div>
-                    </form>
+                                    Sign up
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+
                 </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    } else {
+        return (
+            <>
+                <NavigationBar></NavigationBar>
+                <div className="flex flex-col min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 pt-32">
+                    <div className="text-3xl font-bold text-furious-green">
+                        Success!
+                    </div>
+                    <div>
+                        Redirecting you to the home page...
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
