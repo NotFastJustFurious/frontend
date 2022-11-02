8cffb43f72f09e58f522fb630f6dd905cce08f00
porama <porama@porama.dev>
2022-11-02 21:53:56 +0700

    fix NavBar hover colour

--- src/component/NavigationBar.js
+++ src/component/NavigationBar.js
@@ -37,7 +37,7 @@ export default function NavigationBar(props) {
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
                                 <Disclosure.Button
-                                    className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-furious-green-2 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                                     <span className="sr-only">Open main menu</span>
                                     {open ? (
                                         <XMarkIcon className="block h-6 w-6" aria-hidden="true"/>
@@ -61,7 +61,7 @@ export default function NavigationBar(props) {
                                                 key={item.name}
                                                 href={item.href}
                                                 className={classNames(
-                                                    item.current ? 'bg-gray-900 text-white' : 'hover:bg-white hover:text-white',
+                                                    item.current ? 'bg-gray-900 text-white' : 'hover:bg-furious-green-2 hover:text-white',
                                                     'px-3 py-2 rounded-md text-sm font-medium'
                                                 )}
                                                 aria-current={item.current ? 'page' : undefined}
