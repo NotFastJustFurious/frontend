438712ffdd96a69db7547174ba89e9a716d86378
porama <porama@porama.dev>
2022-11-02 23:01:37 +0700

    fix button in Register page

--- src/page/register/Register.js
+++ src/page/register/Register.js
@@ -102,18 +102,15 @@ export default function Register() {
 
                             </div>
                             <div>
-                                <button
-                                    type="button"
-                                    className="group relative flex w-full justify-center rounded-md border border-transparent bg--600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-00 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                    onClick={onSubmit}
-                                >
 
-                                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <div className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                                         aria-hidden="true"/>
-                                </span>
-                                    Sign up
-                                </button>
+                                <div className="bg-gray-50 text-center">
+                                    <button
+                                        type="submit"
+                                        className="group relative flex w-full justify-center rounded-md border border-transparent bg-furious-green py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                    >
+                                        Sign Up
+                                    </button>
+                                </div>
                             </div>
                         </form>
                     </div>
