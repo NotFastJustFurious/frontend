83169b1b28a915146e0b14d43d5ace475203ab53
pgmnt <mchawyotha@gmail.com>
2022-11-12 15:31:13 +0700

    added messages layout

--- src/page/chat/Chat.js
+++ src/page/chat/Chat.js
@@ -1,7 +1,29 @@
 import NavigationBar from "../../component/NavigationBar";
-import LogCard from "../../component/LogCard";
 
 export default function Chat() {
+
+    let messages = [
+        {
+            id: "1",
+            content: "Hello Opal",
+            time: "10 minutes ago"
+        },
+        {
+            id: "2",
+            content: "Hi Pakgard",
+            time: "12 minutes ago"
+        },
+        {
+            id: "1",
+            content: "Please just kill me already!",
+            time: "24 minutes ago"
+        },
+        {
+            id: "2",
+            content: "Please just kill me already!",
+            time: "24 minutes ago"
+        },
+    ]
 
     return <div className="flex flex-col h-screen">
         <NavigationBar></NavigationBar>
@@ -10,8 +32,18 @@ export default function Chat() {
                 Therapist A
             </div>
         </dev>
-        <LogCard border={false} className="flex-grow"></LogCard>
-
+        <div class="flex flex-col">
+            <div className="p-5 md:mx-20 lg:mx-40">
+              <div className="chat-box">
+                {messages.map((message, time) => (
+                  <div key={time} className={`${message.id === "2" ? 'mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white ml-auto right' : 'left'}  my-3 w-fit shadow-lg p-4 rounded-lg`}>
+                    {/* <span className={`${message.id === "2" ? '  text-green-500' : 'text-orange-500'} rounded font-extrabold`}>{message.id}</span> */}
+                    <p className="md:text-base">{message.content}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+            </div>
         <div className="w-full flex flex-row h-16 p-3.5">
             <input type="text" className="w-2/3 h-full rounded-l-lg"></input>
             <button className="w-1/3 h-full outline outline-1 outline-furious-green-2 rounded-r-lg">Submit</button>
