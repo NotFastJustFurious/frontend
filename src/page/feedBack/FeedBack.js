2a76786e40bcce7817371f29e9a282ebe2ce91b0
sern <sernsuantua@gmail.com>
2022-11-14 15:33:56 +0700

    listbox and updatefeedback added

--- src/page/feedBack/FeedBack.js
+++ src/page/feedBack/FeedBack.js
@@ -2,6 +2,7 @@ import NavigationBar from "../../component/NavigationBar";
 import { useState } from "react";
 import { FaStar } from 'react-icons/fa' // (https://react-icons.github.io/react-icons)
 import { sendAddRecord } from "../../utils/Request";
+import ListBox from "../../component/ListBox";
 
 export default function FeedBack() {
 
@@ -43,30 +44,7 @@ export default function FeedBack() {
                 <div className="flex justify-center item-center">
                     <a className="text-furious-green text-2xl text-bold"> Feedback </a>
                 </div>
-                <div className="max-w-lg mx-auto">
-                    <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button" data-dropdown-toggle="dropdown">Dropdown button <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
-                        <div className="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4" id="dropdown">
-                            <div className="px-4 py-3">
-                                <span className="block text-sm">Bonnie Green</span>
-                                <span className="block text-sm font-medium text-gray-900 truncate">name@flowbite.com</span>
-                            </div>
-                            <ul className="py-1" aria-labelledby="dropdown">
-                                <li>
-                                    <a className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Dashboard</a>
-                                </li>
-                                <li>
-                                    <a className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Settings</a>
-                                </li>
-                                <li>
-                                    <a className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Earnings</a>
-                                </li>
-                                <li>
-                                    <a className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Sign out</a>
-                                </li>
-                            </ul>
-                        </div>
-                </div>
-        
+
                 <div className="flex flex-row justify-center item-center">
                     {stars.map((_, index) => {
                         return (
@@ -84,6 +62,9 @@ export default function FeedBack() {
                             />
                         )
                     })}
+                    <div className="">
+                        <ListBox></ListBox>
+                    </div>
                 </div>
                 <br></br>
                 <div className="flex flex-col justify-center item-center">
