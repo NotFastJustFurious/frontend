fa530bbc5c1c310fb9dceb54d8660ad117a910fc
BungkapTH <www.bung555@gmail.com>
2022-11-15 19:30:46 +0700

    Added anxiety questions and more var for cal score.

--- src/page/mainsurvey/Surveys.js
+++ src/page/mainsurvey/Surveys.js
@@ -19,9 +19,35 @@ export default function Survey() {
         //parse the string value to a int for proper addition
         var anxscore1 = parseInt(getRadioSelectedValue('anx-score-1')),
             anxscore2 = parseInt(getRadioSelectedValue('anx-score-2')),
+            anxscore3 = parseInt(getRadioSelectedValue('anx-score-3')),
+
+            depscore1 = parseInt(getRadioSelectedValue('dep-score-1')),
+            depscore2 = parseInt(getRadioSelectedValue('dep-score-2')),
+            depscore3 = parseInt(getRadioSelectedValue('dep-score-3')),
+
+            ptsdscore1 = parseInt(getRadioSelectedValue('ptsd-score-1')),
+            ptsdscore2 = parseInt(getRadioSelectedValue('ptsd-score-2')),
+            ptsdscore3 = parseInt(getRadioSelectedValue('ptsd-score-3')),
+
+            codscore1 = parseInt(getRadioSelectedValue('cot-score-1')),
+            codscore2 = parseInt(getRadioSelectedValue('cot-score-2')),
+            codscore3 = parseInt(getRadioSelectedValue('cot-score-3')),
+
+            edscore1 = parseInt(getRadioSelectedValue('ed-score-1')),
+            edscore2 = parseInt(getRadioSelectedValue('ed-score-2')),
+            edscore3 = parseInt(getRadioSelectedValue('ed-score-3'));
+
             anxtotal = 0;
+            deptotal = 0;
+            ptsdtotal = 0;
+            codtotal = 0;
+            edtotal = 0;
 
         anxtotal = anxscore1 + anxscore2;
+        deptotal = depscore1 + depscore2;
+        ptsdtotal = ptsdscore1 + ptsdscore2;
+        codtotal = codscore1 + codscore2;
+        edtotal = edscore1 + edscore2;
     };
 
     return (
@@ -65,7 +91,7 @@ export default function Survey() {
                 </div>
 
                 <div className="text-xs w-full flex flex-row justify-between">
-                    Anxiety - Do you feel anxious, worried, or restless?    
+                    Anxiety - Are you easily upset even with the slightest failure?    
                 </div>
                 <div className="text-xs w-full flex flex-row justify-between">
                     <div>
@@ -96,6 +122,40 @@ export default function Survey() {
                         />Always
                     </div>    
                 </div>
+
+                <div className="text-xs w-full flex flex-row justify-between">
+                    Anxiety - Do you feel like you are on edge?
+                </div>
+                <div className="text-xs w-full flex flex-row justify-between">
+                    <div>
+                        <input
+                            type="radio"
+                            value="1"
+                            name="anx-score-3"
+                        />Never
+                        <input 
+                            type="radio" 
+                            value="2" 
+                            name="anx-score-3"
+                        />Rarely
+                        <input 
+                            type="radio" 
+                            value="3" 
+                            name="anx-score-3"
+                        />Sometimes
+                        <input
+                            type="radio" 
+                            value="4" 
+                            name="anx-score-3"
+                        />Often
+                        <input 
+                            type="radio" 
+                            value="5" 
+                            name="anx-score-3"
+                        />Always
+                    </div>    
+                </div>
+
                 <button id="calculate" value="calculate" onClick={onClick}>Confirm</button>
             </div>
         </>
